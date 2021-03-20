b3b38a3682d1033c029d17317c7ba280a08407f1
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2021-03-20 23:49:51 +0900

    SourceFile #1 Upload
    SourceFile #1 Upload: IntroManager

---
+++ IntroManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    float sceneCoolTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1200, true);
+        StartCoroutine(nextScene(sceneCoolTime));
+    }
+
+    IEnumerator nextScene(float coolTime)
+    {
+        yield return new WaitForSeconds(coolTime);
+
+        StopCoroutine(nextScene(coolTime));
+        SceneManager.LoadScene("Title");
+    }
+    
+}
